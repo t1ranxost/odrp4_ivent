44d60fdaa8fcefff96fbe31ba5d7c58983a111fe
T1Ran <roman.gonchukov@gmail.com>
2026-04-17 09:13:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ========== ТВОИ ДАННЫЕ ==========
+here// ========== ТВОИ ДАННЫЕ ==========
 let eventsData = [
     { id: 1, name: "Пол это лава", platform: "T1Ran", organizer: "Нет", date: "13.04.26, 18.30 - 18.45", status: "Проведен", rating: "10.000$", members: 10, callStatus: "🟡Скоро", fullDetails: { description: "Игроки дерутся на кулаках 1 на 1,есть финал,полуфинал", tasks: "", feedback: "", rewards: "", extra: "" } },
     { id: 2, name: "Паркур", platform: "somcop", organizer: "Нет", date: "14.04.26, 19.15 - 19:25", status: "Проведен", rating: "10.000$", members: 9, callStatus: "🟡Скоро", fullDetails: { description: "паркур средней сложности и длины,дается неограниченное количество попыток для прохождения", tasks: "", feedback: "", rewards: "", extra: "" } },
@@ -9,8 +9,7 @@ let eventsData = [
     { id: 7, name: "Слепой убийца", platform: "somcop", organizer: "Нет", date: "15.04.26, 19:15-19:38", status: "Проведен", rating: "10.000$", members: 9, callStatus: "🟡Скоро", fullDetails: { description: "Маньяки охотятся на людей в абсолютно темной комнате", tasks: "", feedback: "", rewards: "", extra: "" } },
     { id: 8, name: "Прятки на даст 2", platform: "somcop", organizer: "Нет", date: "15.04.26, 19:41-19:55", status: "Проведен", rating: "10.000$", members: 11, callStatus: "🟡Скоро", fullDetails: { description: "2 маньяка охотилось за людьми на карте даст 2", tasks: "", feedback: "", rewards: "", extra: "" } },
     { id: 9, name: "Отель маньяка", platform: "somcop", organizer: "Нет", date: "15.04.26, 20:40 - 21:00", status: "Проведен", rating: "10.000$", members: 12, callStatus: "🟡Скоро", fullDetails: { description: "маньяк закрыл в тюрьме человка,задача выживших-спасти человека,найти код для открытия тюрьмы,маньяк охотится за вышившими во время процесса", tasks: "", feedback: "", rewards: "", extra: "" } },
-    { id: 10, name: "Украсть или разделить", platform: "somcop", organizer: "Нет", date: "15.04.26, 21:37 - 21:45", status: "Проведен", rating: "18.000$", members: 2, callStatus: "🟡Скоро", fullDetails: { description: "Игроки выбирают разделить или украсть деньги,если украдет один,получает все,украдут оба,ничего не получат", tasks: "", feedback: "", rewards: "", extra: "" } },
-    { id: 11, name: "Правильная викторина", platform: "Foxy", organizer: "Нет", date: "17.04.26, 12:00 - 12:13", status: "Проведен", rating: "20.000$", members: 2, callStatus: "🟡Скоро", fullDetails: { description: "игроки отвечают на вопросы по серверу правилам и тд", tasks: "", feedback: "", rewards: "", extra: "" } }
+    { id: 10, name: "Украсть или разделить", platform: "somcop", organizer: "Нет", date: "15.04.26, 21:37 - 21:45", status: "Проведен", rating: "18.000$", members: 2, callStatus: "🟡Скоро", fullDetails: { description: "Игроки выбирают разделить или украсть деньги,если украдет один,получает все,украдут оба,ничего не получат", tasks: "", feedback: "", rewards: "", extra: "" } }
 ];
 
 let teamData = [
@@ -754,8 +753,8 @@ function renderAddEventForm() {
                 <small style="color: var(--text-muted);">Кто помогал проводить ивент</small>
             </div>
             <div class="form-group" style="background: var(--badge-bg); border-radius: 20px; padding: 1rem; margin-top: 1rem;">
-                <label style="color: var(--text-primary);">📸 Скриншоты или видео</label>
-                <p style="font-size: 0.85rem; margin-bottom: 0.5rem;">Скиньте фотографии или видео в <strong>ветку бот-отчетность</strong></p>
+                <label style="color: var(--text-primary);">📸 Скриншоты/видео</label>
+                <p style="font-size: 0.85rem; margin-bottom: 0.5rem;">Скиньте фотографии и видео в <strong>ветку бот-отчетность</strong></p>
                 <small style="color: var(--text-muted);">2-3 скриншота или видео, сделанных во время ивента</small>
             </div>
             <button class="submit-btn" id="sendEventToDiscordBtn">📤 Отправить в Discord</button>
@@ -807,7 +806,7 @@ async function sendEventToDiscord() {
     renderTeamTable();
     updateNormStats();
     
-    const webhookURL = "https://discord.com/api/webhooks/1494434308629200906/wzTarhNMe2zTA3x6tO7gn_maa52EXNVcshqse6-2SnKFJ-QulJehmpiCGGWyFjgJ5xgS";
+    const webhookURL = "https://discord.com/api/webhooks/1492180488733851771/aVB2IzG8pZ9aASEjEc7Wa1yjugZcIvHFwmKsRrgHWCZbF0G0ULoZ6tcnx01MlaLi286O";
     
     const embed = {
         title: "📅 Новый ивент",
@@ -821,7 +820,7 @@ async function sendEventToDiscord() {
             { name: "🎁 Призы", value: prizes, inline: false },
             { name: "👤 Организатор", value: organizer, inline: true },
             { name: "🤝 Помощники", value: helpers, inline: true },
-            { name: "📸 Скриншоты или видео", value: "Скинуть в ветку канала!", inline: false }
+            { name: "📸 Скриншоты/видео", value: "Скинуть в ветку канала!", inline: false }
         ],
         footer: { text: "Ивент-отдел UnionTeam" },
         timestamp: new Date().toISOString()
