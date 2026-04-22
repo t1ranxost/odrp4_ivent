6dcfe3e3c513e50f41420652c043923d4b2ec84f
T1Ran <roman.gonchukov@gmail.com>
2026-04-22 08:13:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -533,7 +533,7 @@ let cachedConfig = null;
 async function loadConfig() {
     if (cachedConfig) return cachedConfig;
     
-    const GIST_URL = "https://gist.githubusercontent.com/t1ranxost/aa24e72a4a38ff01eae3b7ee4908cf43/raw/8b9253901e90723dd90112f5749efc3ea9d4d79c/config.json";
+    const GIST_URL = "https://gist.githubusercontent.com/t1ranxost/aa24e72a4a38ff01eae3b7ee4908cf43/raw/43fcf910b062abc6139a80944c91e9e57c3ea98c/config.json";
     try {
         const response = await fetch(GIST_URL);
         const data = await response.json();
